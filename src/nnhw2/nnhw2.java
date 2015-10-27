b6904b80ba3d9036d973b018265a57c50e0f3bee
ymh514 <microvoyager@hotmail.com>
2015-10-27 16:54:30 +0800

    try xor.txt

--- src/nnhw2/nnhw2.java
+++ src/nnhw2/nnhw2.java
@@ -12,99 +12,22 @@ public class nnhw2 extends JFrame {
 
 	static int frameSizeX = 800;
 	static int frameSizeY = 800;
+	static int neuralAmount = 3;
 
 	static ArrayList<float[]> inputArray = new ArrayList<float[]>();
 	static ArrayList<float[]> sortedArray = new ArrayList<float[]>();
 	static ArrayList<float[]> tempArray = new ArrayList<float[]>();
 	static ArrayList<float[]> trainArray = new ArrayList<float[]>();
 	static ArrayList<float[]> testArray = new ArrayList<float[]>();
+	static ArrayList<float[]> initialWeight = new ArrayList<float[]>();
+
+	static float[] yOutput = new float[neuralAmount];
 	
 	static int sortedNewDesire =0;
 
-	public static void printArrayData(ArrayList<float[]> showArray) {
-		for (int i = 0; i < showArray.size(); i++) {
-			for (int j = 0; j < showArray.get(i).length; j++) {
-				System.out.print(showArray.get(i)[j] + "\t");
-			}
-			System.out.println("");
-		}
-	}
-
-	/*
-	 * 1. put first class's type into classTypes 1st place 2. if next line's
-	 * class diffrent with 1st class so go on next if 3. search classTypes's
-	 * all class to judge if all are diffrent rais addFlag 4. if addFlag
-	 * raised, add this new class into classType
-	 */
-	/*
-	private static int countClass(ArrayList<float[]> inputArray) {
-		
-		int addFlag = 0;
-		classTypes.add((int) inputArray.get(0)[(inputArray.get(0).length) - 1]);
-
-		for (int i = 0; i < inputArray.size(); i++) {
-			if (classTypes.get(0) != (int) inputArray.get(i)[(inputArray.get(i).length) - 1]) {
-				for (int j = 0; j < classTypes.size(); j++) {
-					if (classTypes.get(j) != inputArray.get(i)[(inputArray.get(0).length) - 1]) {
-						addFlag = 1;
-					} else {
-						addFlag = 0;
-					}
-				}
-				if (addFlag == 1) {
-					classTypes.add((int) inputArray.get(i)[(inputArray.get(0).length) - 1]);
-				}
-			}
-		}
-		return classTypes.size();
-	}
-*/
-
-	private static void putInputToTemp(ArrayList<float[]> sorteArray) {
-		int arrayInputAmount = sortedArray.size();
-		Random rand = new Random();
-		while (arrayInputAmount != 0) {
-			int n = rand.nextInt(arrayInputAmount) + 0;
-			tempArray.add(sortedArray.get(n));
-			sortedArray.remove(n);// del input to prevent get same data
-			arrayInputAmount--;
-		}
-
-	}
-
-	private static void separateTemp(ArrayList<float[]> tempArray) {
-		int totalamount = tempArray.size();
-		int tocalamount = Math.round((float) (totalamount * 2) / 3);
-		int totestamount = totalamount - tocalamount;
-
-		while (tocalamount != 0) {
-			trainArray.add(tempArray.get(0));
-			tempArray.remove(0);
-			tocalamount--;
-		}
-		System.out.println("train amount : " + trainArray.size());
-		while (totestamount != 0) {
-			testArray.add(tempArray.get(0));
-			tempArray.remove(0);
-			totestamount--;
-		}
-		System.out.println("test amount : " + testArray.size());
-	}
-	private static void genarateFrame(ArrayList<float[]> inputArray, int countClass) {
-		JFrame frame = new JFrame();
-
-		frame.setVisible(true);// just set visible
-		frame.setLocation(100, 100);// set the frame show location
-		frame.setSize(frameSizeX, frameSizeY);// set the frame size
-		frame.setResizable(false);
-
-		Paint trypaint = new Paint(inputArray, countClass);
-		frame.add(trypaint);// add paint(class) things in to the frame
-	}
-
 	public static void inputFileChoose(String[] args) throws IOException {
 
-		String FileName = "/Users/Terry/Documents/workspace/datasets/hw2/wine.txt";
+		String FileName = "/Users/Terry/Documents/workspace/datasets/hw2/xor.txt";
 		FileReader fr = new FileReader(FileName);
 		BufferedReader br = new BufferedReader(fr);// 在br.ready反查輸入串流的狀況是否有資料
 
@@ -133,7 +56,7 @@ public class nnhw2 extends JFrame {
 		fr.close();// 關閉檔案
 
 	}
-
+	
 	public static void sortInputArray(ArrayList<float[]> inputArray) {
 		/*
 		 * 1. set loop times = inputArray's dataamount 
@@ -185,8 +108,127 @@ public class nnhw2 extends JFrame {
 			}
 		}
 		System.out.println("The max sorted desire : "+sortedNewDesire);
+	}	
+	
+	private static void putInputToTemp(ArrayList<float[]> sorteArray) {
+		int arrayInputAmount = sortedArray.size();
+		Random rand = new Random();
+		while (arrayInputAmount != 0) {
+			int n = rand.nextInt(arrayInputAmount) + 0;
+			tempArray.add(sortedArray.get(n));
+			sortedArray.remove(n);// del input to prevent get same data
+			arrayInputAmount--;
+		}
+
 	}
 
+	private static void separateTemp(ArrayList<float[]> tempArray) {
+
+		int totalamount = tempArray.size();
+		int tocalamount = Math.round((float) (totalamount * 2) / 3);
+		int totestamount = totalamount - tocalamount;
+
+		while (tocalamount != 0) {
+			trainArray.add(tempArray.get(0));
+			tempArray.remove(0);
+			tocalamount--;
+		}
+		System.out.println("train amount : " + trainArray.size());
+		while (totestamount != 0) {
+			testArray.add(tempArray.get(0));
+			tempArray.remove(0);
+			totestamount--;
+		}
+		System.out.println("test amount : " + testArray.size());
+	}
+	
+	public static void genarateInitialWeight(){
+		Random rand = new Random();
+		for(int i=0;i<neuralAmount;i++){
+			float[] token = new float[trainArray.size()];
+			for(int j=0 ; j<trainArray.size();j++){
+				token[j]=rand.nextFloat()+1f;
+			}
+			initialWeight.add(token);
+		}	
+	}
+	
+	public static void calOutputValue(ArrayList<float[]> array,ArrayList<float[]> initialWeight){		
+		/*
+		 * 1. use for to run neuralAmount times to get y
+		 * 2. when its last loop get last output 
+		 * 3. use yOutput which generated by upper loop and do cal
+		 *   with weight to get z the last output
+		 *   notice : for(j) loop's yOutput[j-1] cause must 
+		 *            fetch value from the first value
+		 * 4. the latest value of yOutput is outputz
+		 */
+		int x0=-1;
+		for(int i =0;i<neuralAmount;i++){
+			if(i!=neuralAmount-1){
+				float sum=0f;
+				sum=x0*initialWeight.get(i)[0];
+				for(int j=1;j<array.get(i).length;j++){
+					sum += array.get(i)[j]*initialWeight.get(i)[j];
+				}
+				yOutput[i] = (float) (1/(1+Math.exp(-sum)));
+				System.out.println("y"+i+" output is : "+yOutput[i]);
+			}
+			else{
+				float sumZ=0f;
+				sumZ=x0*initialWeight.get(i)[0];
+				for(int j=1;j<yOutput.length;j++){
+					sumZ += yOutput[j-1]*initialWeight.get(i)[j];//must j-1 to match right 
+				}
+				yOutput[i] = (float) (1/(1+Math.exp(-sumZ)));
+				System.out.println("y"+i+"(z) output is : "+yOutput[i]);					
+			}
+		}
+	}
+		
+	private static void genarateFrame(ArrayList<float[]> inputArray, int countClass) {
+		JFrame frame = new JFrame();
+
+		frame.setVisible(true);// just set visible
+		frame.setLocation(100, 100);// set the frame show location
+		frame.setSize(frameSizeX, frameSizeY);// set the frame size
+		frame.setResizable(false);
+
+		Paint trypaint = new Paint(inputArray, countClass);
+		frame.add(trypaint);// add paint(class) things in to the frame
+	}
+	
+	/*
+	 * 1. put first class's type into classTypes 1st place 2. if next line's
+	 * class diffrent with 1st class so go on next if 3. search classTypes's
+	 * all class to judge if all are diffrent rais addFlag 4. if addFlag
+	 * raised, add this new class into classType
+	 */
+	/*
+	private static int countClass(ArrayList<float[]> inputArray) {
+		
+		int addFlag = 0;
+		classTypes.add((int) inputArray.get(0)[(inputArray.get(0).length) - 1]);
+
+		for (int i = 0; i < inputArray.size(); i++) {
+			if (classTypes.get(0) != (int) inputArray.get(i)[(inputArray.get(i).length) - 1]) {
+				for (int j = 0; j < classTypes.size(); j++) {
+					if (classTypes.get(j) != inputArray.get(i)[(inputArray.get(0).length) - 1]) {
+						addFlag = 1;
+					} else {
+						addFlag = 0;
+					}
+				}
+				if (addFlag == 1) {
+					classTypes.add((int) inputArray.get(i)[(inputArray.get(0).length) - 1]);
+				}
+			}
+		}
+		return classTypes.size();
+	}
+*/
+
+	
 	public static void main(String[] args) throws IOException {
 
 		inputFileChoose(args);
@@ -197,11 +239,27 @@ public class nnhw2 extends JFrame {
 
 		separateTemp(tempArray);// separate to train and test set,set 2/3 as
 									// train set 1/3 as test set
+		
 		System.out.println("trainArray's datas : ");
 		printArrayData(trainArray);
+		/*
 		System.out.println("testArray's datas : ");
 		printArrayData(testArray);
-		genarateFrame(trainArray, sortedNewDesire+1);
+		*/
+		genarateInitialWeight();
+		
+		calOutputValue(trainArray,initialWeight);
+//		genarateFrame(trainArray, sortedNewDesire+1);
+	}
+	
+	public static void printArrayData(ArrayList<float[]> showArray) {
+		for (int i = 0; i < showArray.size(); i++) {
+			for (int j = 0; j < showArray.get(i).length; j++) {
+				System.out.print(showArray.get(i)[j] + "\t");
+			}
+			System.out.println("");
+		}
+		System.out.println("");
 	}
 
 }
